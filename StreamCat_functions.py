38daee347fb81f42adfe41937ab11c2cf94c805e
rickD <debboutr@gmail.com>
2016-11-16 16:58:32 -0800

    b4 test pres

--- StreamCat_functions.py
+++ StreamCat_functions.py
@@ -15,6 +15,8 @@
 
 import numpy as np
 import pandas as pd
+import pysal as ps
+from collections import OrderedDict
 ##############################################################################
 
 
@@ -36,3 +38,44 @@ def findUpstreamNpy(zone, com, numpy_dir):
     n = lengths[:itemindex].sum()
     arrlen = lengths[itemindex]
     return upStream[n:n+arrlen]
+
+def makeVPUdict(directory):
+    '''
+    __author__ =  "Rick Debbout <debbout.rick@epa.gov>"
+    Creates an OrderdDict for looping through regions of the NHD to carry InterVPU 
+    connections across VPU zones
+
+    Arguments
+    ---------
+    directory             : the directory contining NHDPlus data at the top level
+    '''
+    B = dbf2DF('%s/NHDPlusGlobalData/BoundaryUnit.dbf' % directory)
+    B = B.drop(B.ix[B.DRAINAGEID.isin(['HI','CI'])].index, axis=0)
+    B = B.ix[B.UNITTYPE == 'VPU'].sort_values('HYDROSEQ',ascending=False)
+    inputs = OrderedDict()  # inputs = OrderedDict((k, inputs[k]) for k in order)
+    for idx, row in B.iterrows():
+        inputs[row.UNITID] = row.DRAINAGEID
+        #print 'HydroRegion (value): ' + row.DRAINAGEID + ' in VPU (key): ' + row.UNITID
+    np.save('%s/StreamCat_npy/zoneInputs.npy' % directory, inputs)
+    return inputs
+    
+    
+    
+    
+def dbf2DF(dbfile, upper=True):
+    '''
+    __author__ = "Ryan Hill <hill.ryan@epa.gov>"
+                 "Marc Weber <weber.marc@epa.gov>"
+    Reads and converts a dbf file to a pandas data frame using pysal.
+
+    Arguments
+    ---------
+    dbfile           : a dbase (.dbf) file
+    '''
+    db = ps.open(dbfile)
+    cols = {col: db.by_col(col) for col in db.header}
+    db.close()  #Close dbf 
+    pandasDF = pd.DataFrame(cols)
+    if upper == True:
+        pandasDF.columns = pandasDF.columns.str.upper() 
+    return pandasDF
